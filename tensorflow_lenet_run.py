b82d21b7a32e70e33cd26a7c29979e61c49f7707
JohnfyBUPT <zhfy888@163.com>
2016-12-12 21:00:32 +0800

    tiny changes

--- tensorflow_lenet_run.py
+++ tensorflow_lenet_run.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn import datasets
 from sklearn.cross_validation import train_test_split
 
+# Preprocessing
 def reformat(dataset, labels):
     dataset = dataset.reshape(
         (-1, 28, 28, 1)).astype(np.float32)
@@ -25,14 +26,13 @@ testData, testLabels = reformat(testData, testLabels)
 print('Training set', trainData.shape, trainLabels.shape)
 print('Test set', testData.shape, testLabels.shape)
 
-
+# constructing the graph
 batch_size = 128
 patch_size = 5
-depth1 = 20
-depth2 = 50
-num_hidden = 64
-num_hidden2 = 32
-image_size = 28
+depth = (20, 50)
+num_hidden = (64, 32)
+image_height = 28
+image_width = 28
 num_labels = 10
 num_channels = 1
 
@@ -42,91 +42,80 @@ with graph.as_default():
 
     # Input data.
     tf_train_dataset = tf.placeholder(
-        tf.float32, shape=(batch_size, image_size, image_size, num_channels))
+        tf.float32, shape=(batch_size, image_height, image_width, num_channels))
     tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
 
     tf_test_dataset = tf.constant(testData)
 
     # First CONV layer variables, in truncated normal distribution.
     layer1_weights = tf.Variable(tf.truncated_normal(
-        [patch_size, patch_size, num_channels, depth1], stddev=0.1))
-    layer1_biases = tf.Variable(tf.zeros([depth1]))
+        [patch_size, patch_size, num_channels, depth[0]], stddev=0.1))
+    layer1_biases = tf.Variable(tf.zeros(depth[0]))
 
     # dropout parameter
     keep_prob = tf.placeholder("float")
 
     # Second CONV layer variables
     layer2_weights = tf.Variable(tf.truncated_normal(
-      [patch_size, patch_size, depth1, depth2], stddev=0.1))
-    layer2_biases = tf.Variable(tf.constant(1.0, shape=[depth2]))
+      [patch_size, patch_size, depth[0], depth[1]], stddev=0.1))
+    layer2_biases = tf.Variable(tf.zeros(depth[1]))
 
     # Three FC layer variables
     layer3_weights = tf.Variable(tf.truncated_normal(
-      [image_size // 4 * image_size // 4 * depth, num_hidden], stddev=0.1))
-    layer3_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
+      [image_size // 4 * image_size // 4 * depth[1], num_hidden[0]], stddev=0.1))
+    layer3_biases = tf.Variable(tf.zeros(num_hidden[0]))
 
     layer4_weights = tf.Variable(tf.truncated_normal(
-      [num_hidden, num_hidden2], stddev=0.1))
-    layer4_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden2]))
+      [num_hidden[0], num_hidden[1]], stddev=0.1))
+    layer4_biases = tf.Variable(tf.zeors(num_hidden[1]))
 
     layer5_weights = tf.Variable(tf.truncated_normal(
-      [num_hidden2, num_labels], stddev=0.1))
-    layer5_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))    
+      [num_hidden[1], num_labels], stddev=0.1))
+    layer5_biases = tf.Variable(tf.zeors(num_labels))    
 
     # Model.
     def model(data):
-        # The convolutional model above uses convolutions with stride 2 to reduce
-        # the dimensionality. Replace the strides by a max pooling operation (nn.max_pool())
-        # of stride 2 and kernel size 2.
-        # I will use 1 x 1 convention then max pooling.
-
-        # layer1
-        # layer1: Conv1 layer
-        # patch 5 * 5 , input_channel 1, depth 16 
-        # A cube of [28,28,16]
-        conv = tf.nn.conv2d(data, layer1_weights, [1, 1, 1, 1], padding='SAME')
-        hidden = tf.nn.relu(conv + layer1_biases)
-
-        # pooling1
-        pool1 = tf.nn.max_pool(hidden, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+        # conv layer1
+        conv1 = tf.nn.conv2d(data, layer1_weights, [1, 1, 1, 1], padding='SAME')
+        hidden1 = tf.nn.relu(conv1 + layer1_biases)
+        pool1 = tf.nn.max_pool(hidden1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
                              padding='SAME', name='pool1')
-        norm1 = tf.nn.lrn(pool1, 4, bias=1.0,   alpha=0.001 / 9.0, beta=0.75,
-                        name='norm1')
+        # Optional: normalization
+        # norm1 = tf.nn.lrn(pool1, 4, bias=1.0,   alpha=0.001 / 9.0, beta=0.75,
+        #                 name='norm1')   
 
-        # layer2
-        conv = tf.nn.conv2d(norm1, layer2_weights, [1, 1, 1, 1], padding='SAME')
-        hidden = tf.nn.relu(conv + layer2_biases)
-        # pooling2
-        pool2 = tf.nn.max_pool(hidden, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
+        # conv layer2
+        conv2 = tf.nn.conv2d(pool1, layer2_weights, [1, 1, 1, 1], padding='SAME')
+        hidden2 = tf.nn.relu(conv2 + layer2_biases)
+        pool2 = tf.nn.max_pool(hidden2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
                              padding='SAME', name='pool1')
-        norm2 = tf.nn.lrn(pool2, 4, bias=1.0,   alpha=0.001 / 9.0, beta=0.75,
-                        name='norm1')
+        # Optional: normalization
+        # norm2 = tf.nn.lrn(pool2, 4, bias=1.0,   alpha=0.001 / 9.0, beta=0.75,
+        #                 name='norm1')
 
-        # norm2, alpha=alpha=0.001 / 100.0;  Test accuracy: 90.3% 
-        # norm2, alpha=alpha=0.1;  Test accuracy: 90.4% 
+        # FC layer1
+        shape = pool2.get_shape().as_list()
+        reshape = tf.reshape(pool2, [shape[0], shape[1] * shape[2] * shape[3]])
+        hidden3 = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
 
-        # layer3
+        # FC layer2
+        hidden4 = tf.matmul(hidden3, layer4_weights) + layer4_biases
 
-        shape = norm2.get_shape().as_list()
-        reshape = tf.reshape(norm2, [shape[0], shape[1] * shape[2] * shape[3]])
-
-        # RELU
-        hidden = tf.nn.relu(tf.matmul(reshape, layer3_weights) + layer3_biases)
-
-        hidden = tf.matmul(hidden, layer4_weights) + layer4_biases
-
-        # add a dropout
+        # Optional: dropout
         # hidden = tf.nn.dropout(hidden, keep_prob)
 
-        result = tf.matmul(hidden, layer5_weights) + layer5_biases
+        # FC layer3
+        result = tf.matmul(hidden4, layer5_weights) + layer5_biases
 
         return result
-  
-    
-    
+
     # Training computation.
     logits = model(tf_train_dataset)
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+
+    # Predictions for the training, validation, and test data.
+    train_prediction = tf.nn.softmax(logits)
+    test_prediction = tf.nn.softmax(model(tf_test_dataset))
 
     # Learning rate decay
     global_step = tf.Variable(0, trainable=False)
@@ -134,13 +123,8 @@ with graph.as_default():
     learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,
                                            100000, 0.96, staircase=True)
 
-
     optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
 
-    # Predictions for the training, validation, and test data.
-    train_prediction = tf.nn.softmax(logits)
-    test_prediction = tf.nn.softmax(model(tf_test_dataset))
-    
 num_steps = 5000
 
 with tf.Session(graph=graph) as session:
@@ -155,7 +139,7 @@ with tf.Session(graph=graph) as session:
         #offset = (step * batch_size) % (trainLabels.shape[0] - batch_size)
         #batch_data = trainData[offset:(offset + batch_size), :, :, :]
         #batch_labels = trainLabels[offset:(offset + batch_size), :]
-        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels,keep_prob:1.0}
+        feed_dict = {tf_train_dataset : batch_data, tf_train_labels : batch_labels, keep_prob:1.0}
         
         _, l, predictions = session.run(
             [optimizer, loss, train_prediction], feed_dict=feed_dict)
