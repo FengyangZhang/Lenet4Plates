967006d0e3c87e5a63b9ba5fe55d2146af11f410
JohnfyBUPT <zhfy888@163.com>
2016-12-13 19:24:19 +0800

    added img2mat

--- data/classGen.py
+++ data/classGen.py
@@ -7,8 +7,8 @@ def main(argv):
     img_names = os.listdir(img_dir)
     file=open('classes.txt','w')  
     if (len(img_names)>0):  
-        for fn in img_names:
-            file.write(fn[0])
+        for name in img_names:
+            file.write(name[0])
             file.write('\n')
 if __name__ == "__main__":
     main(sys.argv[1:])
